66d823e92c05385cd6a0cd1da0e9add025a24548
Mist <114344332+MIST145@users.noreply.github.com>
2024-05-08 09:19:52 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,15 +1,7 @@
---[[
-█▀▀█ █░░█ █▀▀█ █▀▀ █▀▀▄ ░▀░ █░█   █▀▀ ▀▀█▀▀ █░░█ █▀▀▄ ░▀░ █▀▀█
-█░░█ █▀▀█ █░░█ █▀▀ █░░█ ▀█▀ ▄▀▄   ▀▀█ ░░█░░ █░░█ █░░█ ▀█▀ █░░█
-█▀▀▀ ▀░░▀ ▀▀▀▀ ▀▀▀ ▀░░▀ ▀▀▀ ▀░▀   ▀▀▀ ░░▀░░ ░▀▀▀ ▀▀▀░ ▀▀▀ ▀▀▀▀
 
-              PROGRAMADOR: BYBLACKDEATH 
-]]--
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
-name 'PhoenixStudio - Sistema de parilla'
-description 'Sistema de cocina para bares y con trabajo'
-author 'ByBlackDeath'
+
 version 'v2.0'
 
 client_script {
